aa1117ab0a4eb0da76ed60538a93c7823409dace
Sachin Pandit <sachin.pandit@newsignature.com>
2022-02-23 18:27:30 -0500

    initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+
+namespace webapiprovider.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpGet]
+        public string GetCurrentTemprature()
+        {
+            
+            var rng = new Random();
+
+            var temp = rng.Next(20, 90);
+
+            return $"{temp}F";
+        }
+    }
+}
