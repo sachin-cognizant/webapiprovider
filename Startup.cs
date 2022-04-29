fb53c7935abb8ed3c9a2a1ae0c5b81b07e30ed00
sachin-cognizant <91127010+sachin-cognizant@users.noreply.github.com>
2022-04-29 10:55:30 -0400

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,7 @@ namespace webapiprovider
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // test
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
